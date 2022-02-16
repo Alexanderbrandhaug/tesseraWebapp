a639685fd434cde4f48f77fb17b9a004dfb28b27
Ingelin Berglund Rasmussen <ingelin.rasmussen@gmail.com>
2022-02-16 11:31:30 +0100

    halloisen

--- frontend/tessera-webapp/src/Components/logIn.tsx
+++ frontend/tessera-webapp/src/Components/logIn.tsx
@@ -1,1 +1,44 @@
 import React from "react";
+import { useState } from "react";
+
+
+export default function NameForm() {
+
+    function useInput(initialValue: string){
+        const [value, setValue] = useState(initialValue);
+        return {
+          value,
+          setValue,
+          reset: () => setValue(""),
+          bind: {
+            value,
+            onChange: (event: any) => {
+              setValue(event.target.value);
+            }
+          }
+        };
+      };
+
+    const { value:firstName, bind:bindFirstName, reset:resetFirstName } = useInput('');
+    const { value:password, bind:bindPassword, reset:resetPassword } = useInput('');
+    
+    const handleSubmit = (evt: any) => {
+        evt.preventDefault();
+        alert(`Submitting Name ${firstName} ${password}`);
+        resetFirstName();
+        resetPassword();
+    }
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" {...bindFirstName} />
+        </label>
+        <label>
+          Password:
+          <input type="text" {...bindPassword} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
--- frontend/tessera-webapp/src/Pages/logInPage.tsx
+++ frontend/tessera-webapp/src/Pages/logInPage.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import React from "react";
+import LogIn from "../Components/logIn";
 
 export default function LogInPage() {
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Log in</h2>
+      <div>
+        <nav>
+        <LogIn />
+        </nav>
+      </div>
     </main>
   );
 }
