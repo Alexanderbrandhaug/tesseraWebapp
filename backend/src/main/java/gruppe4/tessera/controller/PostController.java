85dc6cc6fcc3c997ad8aa7523dfa13358ab1c3f5
Alexander <alexabr@ntnu.no>
2022-02-16 13:48:51 +0100

    added cors policy fix

--- backend/src/main/java/gruppe4/tessera/controller/PostController.java
+++ backend/src/main/java/gruppe4/tessera/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import gruppe4.tessera.service.PostService;
 
 @Controller
 @RequestMapping(path = "/tessera/api")
+@CrossOrigin("*")
 public class PostController {
     
 
