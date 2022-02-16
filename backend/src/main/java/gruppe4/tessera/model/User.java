667bb827005e9876efd06a8f415e45e92062654d
jesperba <jespbarf@gmail.com>
2022-02-16 10:52:36 +0100

    added lombok functionality, and corrected some of the fields

--- backend/src/main/java/gruppe4/tessera/model/User.java
+++ backend/src/main/java/gruppe4/tessera/model/User.java
@@ -8,56 +8,40 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Table
-@Entity(name = "users") // This tells Hibernate to make a table out of this class
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;    
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "users")    // TODO Table vs Entity
+@NoArgsConstructor // TODO ?
+@AllArgsConstructor
+@Data
+
 public class User {
-  @Id
+  @Id //TODO fjerne?    Hvordan flytte det til username?
   @GeneratedValue(strategy=GenerationType.AUTO) // ID will be used as primarykey and will be autoincremented
+  
   private Integer id;
-  private String name;
+  private String username;
+  private String description;
+
+  @Column(name = "creation_date", nullable = false)
+  private LocalDate creationDate;
+
+  @Column(name = "suspended")
+  private boolean isSuspended;
 
   @Column(unique=true, nullable = false) // tells the DB that email variable is going to be unique and not nullable in the DB
-  private String email;
-  private String password;
+  private String password;  //unique?
   @Column(name = "is_admin")
   private boolean isAdmin;
-  
 
-  public Integer getId() {
-    return id;
-  }
-
-  public void setAdmin(boolean isAdmin){
-    this.isAdmin = isAdmin;
-  }
   @JsonProperty("admin")
   public boolean getRole(){
     return isAdmin;
   }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-  public void setPassword(String password){
-    this.password = password;
-  }
-  public String getPassword(){
-    return password;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
 }
