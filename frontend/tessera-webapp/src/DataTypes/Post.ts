9acb21608eae9918d9a3c16e585d3d04469d4066
Henrik Sontvedt <henriksontvedt@gmail.com>
2022-02-16 12:06:36 +0100

    Add Post data-type for future handling of post-data. Change feed css and to support Post data-type. Change getPosts to support Post data-type

--- frontend/tessera-webapp/src/DataTypes/Post.ts
+++ frontend/tessera-webapp/src/DataTypes/Post.ts
@@ -1,1 +1,30 @@
-export {} 
+export {Post} 
+
+
+class Post {
+    id: number;
+    userID: number;
+    title: string;
+    location: string;
+    description: string;
+    createdAt: string;
+    price: number;
+    contactPoint: string; 
+    active: string;
+    postType: string;
+    eventType: string;
+    
+    constructor(id: number, userID: number, title: string, location: string, description: string, createdAt: string, price: number,  contactPoint: string, active: string, postType: string, eventType: string){
+        this.id = id;
+        this.userID = userID;
+        this.title = title;
+        this.location = location;
+        this.description = description
+        this.createdAt = createdAt
+        this.price = price;
+        this.contactPoint = contactPoint;
+        this.active = active
+        this.postType = postType
+        this.eventType = eventType
+    }
+}
