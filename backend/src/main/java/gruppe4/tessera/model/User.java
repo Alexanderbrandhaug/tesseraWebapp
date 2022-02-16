79c73f332560f052c8a20565cd7a31114cee48ef
jesperba <jespbarf@gmail.com>
2022-02-16 11:06:45 +0100

    cleaned up code in User.java

--- backend/src/main/java/gruppe4/tessera/model/User.java
+++ backend/src/main/java/gruppe4/tessera/model/User.java
@@ -15,28 +15,32 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;    
 
 @Entity // This tells Hibernate to make a table out of this class
-@Table(name = "users")    // TODO Table vs Entity
-@NoArgsConstructor // TODO ?
+@Table(name = "users")
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 
 public class User {
-  @Id //TODO fjerne?    Hvordan flytte det til username?
+  @Id
   @GeneratedValue(strategy=GenerationType.AUTO) // ID will be used as primarykey and will be autoincremented
   
   private Integer id;
+
+  @Column(name = "username", unique = true, nullable = false)
   private String username;
   private String description;
 
   @Column(name = "creation_date", nullable = false)
   private LocalDate creationDate;
 
-  @Column(name = "suspended")
+  private String profilePicture;
+
+  @Column(name = "is_suspended", nullable = false)
   private boolean isSuspended;
 
-  @Column(unique=true, nullable = false) // tells the DB that email variable is going to be unique and not nullable in the DB
-  private String password;  //unique?
-  @Column(name = "is_admin")
+  @Column(nullable = false) // tells the DB that email variable is going to be unique and not nullable in the DB
+  private String password;
+  @Column(name = "is_admin", nullable = false)
   private boolean isAdmin;
 
   @JsonProperty("admin")
