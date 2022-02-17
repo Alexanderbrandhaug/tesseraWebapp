618bd88c2ef5e12dd9886d341b7a7bf07b6800bc
TommyTom Frettis <thomas.frette@gmail.com>
2022-02-17 14:45:28 +0100

    Add site for posts. Add information about the post and a button linked to the users profile.

--- frontend/tessera-webapp/src/Pages/postpage.tsx
+++ frontend/tessera-webapp/src/Pages/postpage.tsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { Post } from "../DataTypes/Post";
 import { getPosts } from "../Utility/data";
+import { useNavigate } from "react-router-dom";
 
 export default function PostPage() {
+  const navigate = useNavigate();
   const location = useLocation();
   const [post, setPost] = useState<Post | null>(null);
 
@@ -22,13 +24,30 @@ export default function PostPage() {
     }
   }, [location.pathname])
 
+  function redirect() {
+    navigate("/profile/" + post?.userID)
+  }
+
   return (
     <main style={{ padding: "1rem 0" }}>
       {
       post !== null ? 
 
       <div>
-        VI FANT EN POST!
+        <header>
+          <h1> {post.title}</h1>
+        </header>
+        <body>
+          <p>Location: {post.location}</p>
+          <p>Date: {post.createdAt}</p>
+          <p>Price: {post.price}</p>
+          <p>Contact {post.contactPoint}</p>
+
+
+          <p>Beskrivelse</p>
+          <p>{post.description}</p>
+          <button onClick={redirect}>See user</button>
+        </body>
       </div>
       :
       <div>
