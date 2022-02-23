559656eb1bb08db47cdec75419a245e68ab5a52a
Alexander <alexabr@ntnu.no>
2022-02-23 11:56:34 +0100

    designed login UI

--- frontend/tessera-webapp/src/Pages/logInPage.tsx
+++ frontend/tessera-webapp/src/Pages/logInPage.tsx
@@ -5,7 +5,6 @@ import LogIn from "../Components/logIn";
 export default function LogInPage() {
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Log in</h2>
       <div>
         <nav>
         <LogIn />
