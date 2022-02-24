486768e7f7d0d4b66ac7661d7b106d7ce95dcd08
Lavrans Blytt <lavransb@stud.ntnu.no>
2022-02-24 13:28:21 +0100

    Changed handling of login to localstorage

--- frontend/tessera-webapp/src/App.tsx
+++ frontend/tessera-webapp/src/App.tsx
@@ -1,13 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
-import {useState} from 'react';
 import './App.css';
 import LogInPage from "./Pages/logInPage";
 
 function App() {
-  const [token, setToken] = useState("");
+  const token = localStorage.getItem('user');
+
+  function handleLogOut() {
+    localStorage.removeItem('user');
+    window.location.reload();
+  }
 
   if (!token) {
-    return <LogInPage setToken={setToken} />
+    return <LogInPage />
   }
 
   return (
@@ -21,6 +25,9 @@ function App() {
       >
         <Link to="/feed">Feed</Link> |{" "}
         <Link to="/profile">Profile</Link>|{" "}
+        <button type="button" onClick={handleLogOut}>
+          Log Out
+        </button>
       </nav>
       <Outlet />
     </div>
