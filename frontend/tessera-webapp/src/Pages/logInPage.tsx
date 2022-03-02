863d19b401ea42d61ce97d92745c12816ab68ee1
TommyTom Frettis <thomas.frette@gmail.com>
2022-03-02 08:36:16 +0100

    finalize the styling of loginpage

--- frontend/tessera-webapp/src/Pages/logInPage.tsx
+++ frontend/tessera-webapp/src/Pages/logInPage.tsx
@@ -8,19 +8,26 @@ import Footer from '../Components/footer';
 export default function LogInPage() {
 
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <div>
-      <header className="header">
-        <h1>Tessera</h1>
-      </header>
-      <h2 className="loginText">Just a ticket away</h2>
-      {/*<img alt='ticketlogo' src={icon} className="ticketIcon"/>*/}
-      <h5 className="loginText2">The Marketplace for trading tickets near you!</h5>
-        <nav>
-        <LogIn />
-        </nav>
-      </div>
-      <Footer></Footer>
+    <main>
+      <div className="loginPage">
+        <div className="loginLeftColumn">
+          <header className="header">
+            <h1>Tessera</h1>
+          </header>
+          <div className="infoBox">
+            <h2 className="loginText">Just a ticket away</h2>
+            <h5 className="loginText2">The Marketplace for trading tickets near you!</h5>
+          </div>
+        </div>
+
+        <div className = "loginRightColumn">
+          <div className="loginBox">
+            <LogIn />
+          </div>
+        </div>
+    </div>
+    
+    <Footer />
     </main>
   );
 }
