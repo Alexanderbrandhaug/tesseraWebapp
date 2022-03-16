6d61e84d99aac60ab2fa29637bd013715af6195a
victoriabi <victorbi@stud.ntnu.no>
2022-03-16 09:31:39 +0100

    started on the profile page

--- frontend/tessera-webapp/src/Pages/profile.tsx
+++ frontend/tessera-webapp/src/Pages/profile.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import Tally from "../Components/Tally";
+import { getUser } from "../Utility/data";
 
 export default function ProfilePage() {
 
+  const [username, setUsername] = useState(localStorage.getItem("username") ?? "");
+  const [description, setDescription] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  async function getDescription() {
+    getUser(username).then(response => {
+      if (response) {
+        setDescription(response.data.description)
+      } else {
+        setErrorMessage("Cannot find user. ")
+      }
+    })
+  } 
+
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Profile</h2>
+      <h2>Profile {{username}}</h2>
     </main>
   );
 }
